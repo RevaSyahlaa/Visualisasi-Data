e62e80a988dfc6edb383deec7374c997fd1b98bd
RevaSyahlaa <114666789+RevaSyahlaa@users.noreply.github.com>
2022-10-26 20:38:38 +0700

    25 contoh visualisai data

---
+++ 15. plot pie2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+labels = ['Lain-lain','Samsung','Apple','Huawei','Oppo','Vivo']
+values = [42,21,14,9,8,7]
+colors = ['red','green','pink','blue','yellow','orange']
+explode = [0,0.3,0,0,0,0]
+plt.title('Diagram Pie Market Share Smartphone')
+plt.pie(values,labels=labels,colors=colors,explode=explode,shadow=True,autopct='%1.1f%%',startangle=180)
+plt.axis('equal')
+plt.show()
